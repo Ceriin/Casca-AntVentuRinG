2338f77c5b86e51e120aadaa604117485c62f32d
Casca <jandriehaus@gmail.com>
2023-02-01 22:34:51 +0100

    Created tile_data.js

---
+++ assets/js/tile_data.js
@@ -0,0 +1,17 @@
+let tile_data = {
+    top_left_corner: new Tile('assets/img/tiles/room_border_corner_v1.png'),
+    top_right_corner: new Tile('assets/img/tiles/room_border_corner_v2.png'),
+    bottom_right_corner: new Tile('assets/img/tiles/room_border_corner_v3.png'),
+    bottom_left_corner: new Tile('assets/img/tiles/room_border_corner_v4.png'),
+    top_edge: new Tile('assets/img/tiles/room_border_side_v1.png'),
+    right_edge: new Tile('assets/img/tiles/room_border_side_v2.png'),
+    bottom_edge: new Tile('assets/img/tiles/room_border_side_v3.png'),
+    left_edge: new Tile('assets/img/tiles/room_border_side_v4.png'),
+    top_open: new Tile('assets/img/tiles/room_border_open_v1.png'),
+    right_open: new Tile('assets/img/tiles/room_border_open_v2.png'),
+    bottom_open: new Tile('assets/img/tiles/room_border_open_v3.png'),
+    left_open: new Tile('assets/img/tiles/room_border_open_v4.png'),
+    wooden_floor_one: new Tile('assets/img/tiles/wooden_floor_v1.png'),
+    wooden_floor_two: new Tile('assets/img/tiles/wooden_floor_v1.png'),
+    wooden_floor_three: new Tile('assets/img/tiles/wooden_floor_v1.png')
+}
